a835653d985b86e88eb274ba2c9228fdcf3417cf
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-02-17 22:19:56 -0300

    chore: implemented basic interface to handle Note repository

--- packages/server/src/repositories/INoteRepository.ts
+++ packages/server/src/repositories/INoteRepository.ts
@@ -3,7 +3,7 @@ import { Note } from '../entities/Note';
 export interface INoteRepository {
   findById(): Promise<Note>;
   list(): Promise<Note[]>;
-  save(note: Note): Promise<void>;
+  save(note: Note): Promise<Note>;
   update: Promise<void>;
   delete: Promise<void>;
 }
