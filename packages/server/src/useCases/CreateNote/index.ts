89de022e665d971c88e4921cb97aa4976f9670fb
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-03-02 23:15:49 -0300

    chore: changed implementation to debug implementation

--- packages/server/src/useCases/CreateNote/index.ts
+++ packages/server/src/useCases/CreateNote/index.ts
@@ -1,10 +1,12 @@
-import { NoteRepository } from '../../repositories/implementations/DebugRepository';
+// import { NoteRepository } from '../../repositories/implementations/DebugRepository';
+import { PostgresRepository } from '../../repositories/implementations/PostgresRepository';
 import { CreateNoteController } from './CreateNoteController';
 import { CreateNoteUseCase } from './CreateNoteUseCase';
 
-const createNoteRepository = new NoteRepository();
+// const createNoteRepository = new NoteRepository();
+const postgresRepository = new PostgresRepository();
 
-const createNoteUseCase = new CreateNoteUseCase(createNoteRepository);
+const createNoteUseCase = new CreateNoteUseCase(postgresRepository);
 
 const createNoteController = new CreateNoteController(createNoteUseCase);
 
